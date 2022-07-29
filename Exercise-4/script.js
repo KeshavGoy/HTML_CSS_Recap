917e7c93c784aa6e5b66a7dcc00f30e660438525
Keshav <keshavgoyal2001@gmail.com>
2022-07-29 23:12:25 +0530

    Mark Imp. functionality added

--- Exercise-4/script.js
+++ Exercise-4/script.js
@@ -34,6 +34,7 @@ addBtn.addEventListener('click',function(e){
 
 function showNotes(){
     let notes = localStorage.getItem("notes");
+    let imp = localStorage.getItem("imp");
     if(notes==null)
     {
         notesObj = [];
@@ -41,16 +42,50 @@ function showNotes(){
     else{
         notesObj = JSON.parse(notes);
     }
+
     let cards = "";
+
+
+    // checking important notes and displaying them first
+    if(imp==null)
+    {
+        // console.log("No imp");
+        impObj=[];
+    }
+    else{
+        impObj = JSON.parse(imp);
+        impObj.forEach(function(element){
+            let content = notesObj[element];
+            cards += `
+        <div class="card" style="background-color:antiquewhite">
+            <h2 class="space card-title">${content[1]}</h2>
+            <pre class="space card-content">${content[0]}</pre>
+            <div>
+                <button onclick="deleteNote(this.id)" class="button space" id="${element}" type="submit">Delete Note</button>
+                <button onclick="markImp(this.id.slice(8))" class="button space" id="markImp_${element}">Mark Normal</buttoon>
+            </div>
+        </div>
+        `;
+
+        });
+    }
+
+
     notesObj.forEach(function(element,index) {
         // console.log(element[0]);
+        if(!impObj.includes(index))
+        {    
         cards += `
         <div class="card">
             <h2 class="space card-title">${element[1]}</h2>
             <pre class="space card-content">${element[0]}</pre>
-            <button onclick="deleteNote(this.id)" class="button space" id="${index}" type="submit">Delete Note</button>
+            <div>
+                <button onclick="deleteNote(this.id)" class="button space" id="${index}" type="submit">Delete Note</button>
+                <button onclick="markImp(this.id.slice(8))" class="button space" id="markImp_${index}">Mark Imp</buttoon>
+            </div>
         </div>
         `;
+        }
 
     });
     let notesElem = document.querySelector('.Notes-Area');
@@ -94,3 +129,35 @@ search.addEventListener('input',function(e){
         }
     })
 });
+
+// Marking Notes as Imp
+function markImp(index_s){
+    index = Number(index_s);
+    let MarkImp = document.getElementById("markImp_"+index_s);
+    let card = MarkImp.parentNode;
+        // console.log(MarkImp);
+    let imp = localStorage.getItem("imp");
+    if(imp==null)
+    {
+        impObj = [];
+    }
+    else{
+        impObj = JSON.parse(imp);
+    }
+    if(impObj.includes(index))
+    {
+        impObj.splice(impObj.indexOf(index),1);
+        localStorage.setItem("imp",JSON.stringify(impObj));   
+        MarkImp.innerText = "Mark Imp";
+        card.style.backgroundColor="white";
+    }
+    else
+    {
+        impObj.push(index);
+        impObj.sort();
+        localStorage.setItem("imp",JSON.stringify(impObj));
+        card.style.backgroundColor = "antiqueWhite";
+        MarkImp.innerHTML = "Mark Norm";
+    }
+    showNotes();
+}
